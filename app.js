24113e04754d0c642714439736ff1dce57c12e7a
Ginn <82102687+ZenithGn@users.noreply.github.com>
2021-12-31 20:23:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,258 +1,58 @@
-var countDownDate = new Date("Dec 31, 2021 12:00:00").getTime();
-
-var x = setInterval(function() {
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  if (distance >=0){
-    document.getElementById("title").innerHTML="New Year Countdown";
-    document.getElementById("text1").innerHTML="days";
-    document.getElementById("text2").innerHTML="hours";
-    document.getElementById("text3").innerHTML="minutes";
-    document.getElementById("text4").innerHTML="seconds";
-    document.getElementById("days").innerHTML =days;
-    document.getElementById("hours").innerHTML=hours ;
-    document.getElementById("minutes").innerHTML=minutes ;
-    document.getElementById("seconds").innerHTML=seconds ;
-  }
-  else{
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "Happy New Year";
-  }
-}, 1000);
-function check(newyear) {
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  if (distance < 0) {
-    return true;
-  }
-}
-let newyear;
-if (check(newyear) == true) {
-  window.requestAnimFrame = (function() {
-    return (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      }
-    );
-  })();
-
-  var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
-    cw = window.innerWidth,
-    ch = window.innerHeight,
-    fireworks = [],
-    particles = [],
-    hue = 120,
-    limiterTotal = 25,
-    limiterTick = 1,
-    timerTotal = 20,
-    timerTick = 0,
-    mousedown = false,
-    mx,
-    my;
-  canvas.width = cw;
-  canvas.height = ch;
-
-  function random(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  function calculateDistance(p1x, p1y, p2x, p2y) {
-    var xDistance = p1x - p2x,
-      yDistance = p1y - p2y;
-    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-  }
-
-  function Firework(sx, sy, tx, ty) {
-    this.x = sx;
-    this.y = sy;
-    this.sx = sx;
-    this.sy = sy;
-    this.tx = tx;
-    this.ty = ty;
-    this.distanceToTarget = calculateDistance(sx, sy, tx, ty);
-    this.distanceTraveled = 0;
-    this.coordinates = [];
-    this.coordinateCount = 3;
-    while (this.coordinateCount--) {
-      this.coordinates.push([this.x, this.y]);
-    }
-    this.angle = Math.atan2(ty - sy, tx - sx);
-    this.speed = 2;
-    this.acceleration = 1.05;
-    this.brightness = random(50, 70);
-    this.targetRadius = 1;
-  }
-
-  Firework.prototype.update = function(index) {
-    this.coordinates.pop();
-    this.coordinates.unshift([this.x, this.y]);
-    if (this.targetRadius < 8) {
-      this.targetRadius += 0.3;
-    } else {
-      this.targetRadius = 1;
-    }
-
-    this.speed *= this.acceleration;
-    var vx = Math.cos(this.angle) * this.speed,
-      vy = Math.sin(this.angle) * this.speed;
-    this.distanceTraveled = calculateDistance(
-      this.sx,
-      this.sy,
-      this.x + vx,
-      this.y + vy
-    );
-
-    if (this.distanceTraveled >= this.distanceToTarget) {
-      createParticles(this.tx, this.ty);
-      fireworks.splice(index, 1);
-    } else {
-      this.x += vx;
-      this.y += vy;
-    }
+function timeLeft(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
   };
+};
 
-  Firework.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.moveTo(
-      this.coordinates[this.coordinates.length - 1][0],
-      this.coordinates[this.coordinates.length - 1][1]
-    );
-    ctx.lineTo(this.x, this.y);
-    ctx.strokeStyle = "hsl(" + hue + ", 100%, " + this.brightness + "%)";
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(this.tx, this.ty, this.targetRadius, 0, Math.PI * 2);
-    ctx.stroke();
+$(document).ready(function() {
+  var today = new Date();
+  var deadline = 'January 1 ' + (today.getFullYear() + 1) + " 00:00:00";
+  if (today.getMonth() == 0 && today.getDate() == 1) {
+    deadline = 'January 1 ' + (today.getFullYear()) + " 00:00:00";
   };
-
-  function Particle(x, y) {
-    this.x = x;
-    this.y = y;
-    this.coordinates = [];
-    this.coordinateCount = 5;
-    while (this.coordinateCount--) {
-      this.coordinates.push([this.x, this.y]);
-    }
-    this.angle = random(0, Math.PI * 2);
-    this.speed = random(1, 10);
-    this.friction = 1;
-    this.gravity = 3.9;
-    this.hue = random(hue - 50, hue + 50);
-    this.brightness = random(50, 80);
-    this.alpha = 1;
-    this.decay = random(0.015, 0.03);
-  }
-
-  Particle.prototype.update = function(index) {
-    this.coordinates.pop();
-    this.coordinates.unshift([this.x, this.y]);
-    this.speed *= this.friction;
-    this.x += Math.cos(this.angle) * this.speed;
-    this.y += Math.sin(this.angle) * this.speed + this.gravity;
-    this.alpha -= this.decay;
-
-    if (this.alpha <= this.decay) {
-      particles.splice(index, 1);
-    }
-  };
-
-  Particle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.moveTo(
-      this.coordinates[this.coordinates.length - 1][0],
-      this.coordinates[this.coordinates.length - 1][1]
-    );
-    ctx.lineTo(this.x, this.y);
-    ctx.strokeStyle =
-      "hsla(" +
-      this.hue +
-      ", 100%, " +
-      this.brightness +
-      "%, " +
-      this.alpha +
-      ")";
-    ctx.stroke();
-  };
-
-  function createParticles(x, y) {
-    var particleCount = 30;
-    while (particleCount--) {
-      particles.push(new Particle(x, y));
-    }
-  }
-
-  function loop() {
-    requestAnimFrame(loop);
-
-    hue = random(0, 360);
-    ctx.globalCompositeOperation = "destination-out";
-    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
-    ctx.fillRect(0, 0, cw, ch);
-    ctx.globalCompositeOperation = "lighter";
-    var i = fireworks.length;
-    while (i--) {
-      fireworks[i].draw();
-      fireworks[i].update(i);
-    }
-    var i = particles.length;
-    while (i--) {
-      particles[i].draw();
-      particles[i].update(i);
-    }
-
-    if (timerTick >= timerTotal) {
-      if (!mousedown) {
-        fireworks.push(
-          new Firework(cw / 2, ch, random(0, cw), random(0, ch / 2))
-        );
-        timerTick = 0;
-      }
-    } else {
-      timerTick++;
-    }
-
-    if (limiterTick >= limiterTotal) {
-      if (mousedown) {
-        fireworks.push(new Firework(cw / 2, ch, mx, my));
-        limiterTick = 0;
-      }
-    } else {
-      limiterTick++;
-    }
-  }
-
-  canvas.addEventListener("mousemove", function(e) {
-    mx = e.pageX - canvas.offsetLeft;
-    my = e.pageY - canvas.offsetTop;
+  
+  $("#header").hover(function() {
+    $(this).toggleClass('bluelight');
   });
-
-  canvas.addEventListener("mousedown", function(e) {
-    e.preventDefault();
-    mousedown = true;
+  
+  $(".clock").hover(function() {
+    $(this).toggleClass('bluelight');
   });
-
-  canvas.addEventListener("mouseup", function(e) {
-    e.preventDefault();
-    mousedown = false;
-  });
-
-  window.onload = loop;
-}
+  
+  var setClock = function(newyear){
+    var timeinterval = setInterval(function(){
+      var t = timeLeft(newyear);
+      $('#days').text(t.days);
+      $('#hours').text(t.hours);
+      $('#mins').text(('0' + t.minutes).slice(-2));
+      $('#secs').text(('0' + t.seconds).slice(-2));
+      if(t.total<=0){
+        clearInterval(timeinterval);
+        var now = new Date();
+        var yearStr = now.getFullYear().toString();
+        $('#header').text("Happy New Year!!!");
+        $('#days').text(yearStr[0]);
+        $('#days-text').text("Happy");
+        $('#hours').text(yearStr[1]);
+        $('#hours-text').text("New");
+        $('#mins').text(yearStr[2]);
+        $('#mins-text').text("Year");
+        $('#secs').text(yearStr[3]);
+        $('#secs-text').text("!!!");
+        $('#info').text("Countdown starts again tomorrow!");
+      }
+    },1000);
+  };
+  
+  setClock(deadline);
+  
+});
